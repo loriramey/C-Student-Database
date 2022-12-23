620428ba8733f080ff2c6f26921b9771122767c4
loriramey <loriramey@hotmail.com>
2022-12-23 12:06:54 -0500

    Initial Commit

---
+++ C867 LRamey/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C867 LRamey
+//
+//  Created by Lori Ramey on 12/23/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
